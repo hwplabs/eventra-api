e2fbf055a83676de593f27b14af8878211b0600c
akandeseun <akandeseun44@gmail.com>
2023-06-05 13:21:14 +0100

    sequelize config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
 import { AuthModule } from "./auth/auth.module"
 import { ConfigModule } from "@nestjs/config"
+import { SequelizeModule } from "@nestjs/sequelize"
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot()],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      models: [],
+      // autoLoadModels:true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
