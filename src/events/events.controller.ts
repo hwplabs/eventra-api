62d50f543cc8fa37f5ab63767d8ecc9e9277b480
akandeseun <akandeseun44@gmail.com>
2023-06-13 13:13:24 +0100

    w.i.p: events

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,8 +1,61 @@
-import { Body, Controller, Patch } from "@nestjs/common"
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+  ValidationPipe,
+} from "@nestjs/common"
 import { EventsService } from "./events.service"
 import { UpdateEventDto } from "./dto/update-event.dto"
+import { Event } from "./models/event.model"
+import { CreateEventDto } from "./dto/create-event.dto"
+import { FileInterceptor } from "@nestjs/platform-express"
+import { AuthGuard } from "@nestjs/passport"
 
 @Controller("events")
+// @UseGuards(AuthGuard())
 export class EventsController {
   constructor(private eventService: EventsService) {}
+
+  @Get()
+  getAllEvents(): Promise<Event[]> {
+    return this.eventService.getEvents()
+  }
+
+  @Get(":id")
+  getEvent(@Param("id") id: string): Promise<Event> {
+    return this.eventService.getEventById(id)
+  }
+
+  @Post()
+  @UseInterceptors(FileInterceptor("eventImage"))
+  createEvent(
+    @Body(ValidationPipe) createEventDto: CreateEventDto,
+    @UploadedFile() eventImage: Express.Multer.File,
+  ): Promise<Event> {
+    return this.eventService.createEvent(createEventDto, eventImage)
+  }
+
+  @Patch(":id")
+  @UseInterceptors(FileInterceptor("eventImage"))
+  updateEvent(
+    @Param("id") id: string,
+    @Body(ValidationPipe) updateEventDto: UpdateEventDto,
+    @UploadedFile() eventImage?: Express.Multer.File,
+  ): Promise<Event> {
+    return this.eventService.updateEvent(id, updateEventDto, eventImage)
+  }
+
+  @Delete(":id")
+  @HttpCode(204)
+  deleteEvent(@Param("id") id: string) {
+    return this.eventService.deleteEvent(id)
+  }
 }
--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -3,9 +3,10 @@ import { EventsController } from "./events.controller"
 import { EventsService } from "./events.service"
 import { SequelizeModule } from "@nestjs/sequelize"
 import { Event } from "./models/event.model"
+import { AuthModule } from "src/auth/auth.module"
 
 @Module({
-  imports: [SequelizeModule.forFeature([Event])],
+  imports: [SequelizeModule.forFeature([Event]), AuthModule],
   controllers: [EventsController],
   providers: [EventsService],
 })
