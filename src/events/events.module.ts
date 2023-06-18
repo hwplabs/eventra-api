ea2a42dbb5fc18911449f33019a5168e522d0f7f
akandeseun <akandeseun44@gmail.com>
2023-06-18 13:36:05 +0100

    w.i.p

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -5,9 +5,15 @@ import { SequelizeModule } from "@nestjs/sequelize"
 import { Event } from "./models/event.model"
 import { AuthModule } from "src/auth/auth.module"
 import { CategoryModule } from "src/category/category.module"
+import { OrganiserModule } from "src/organiser/organiser.module"
 
 @Module({
-  imports: [SequelizeModule.forFeature([Event]), AuthModule, CategoryModule],
+  imports: [
+    SequelizeModule.forFeature([Event]),
+    AuthModule,
+    CategoryModule,
+    OrganiserModule,
+  ],
   controllers: [EventsController],
   providers: [EventsService],
   exports: [SequelizeModule],
--- src/organiser/organiser.module.ts
+++ src/organiser/organiser.module.ts
@@ -4,10 +4,12 @@ import { OrganiserService } from "./organiser.service"
 import { SequelizeModule } from "@nestjs/sequelize"
 import { Organiser } from "./models/organiser.model"
 import { AuthModule } from "src/auth/auth.module"
+import { Event } from "src/events/models/event.model"
 
 @Module({
-  imports: [SequelizeModule.forFeature([Organiser]), AuthModule],
+  imports: [SequelizeModule.forFeature([Organiser, Event]), AuthModule],
   controllers: [OrganiserController],
   providers: [OrganiserService],
+  exports: [SequelizeModule],
 })
 export class OrganiserModule {}
