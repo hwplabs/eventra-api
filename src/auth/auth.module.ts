b0cea9873c6da69be3b530593a56d26cf6bddee2
akandeseun <akandeseun44@gmail.com>
2023-07-26 16:41:09 +0100

    w.i.p: fixxxxx

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,15 @@
 import { Module } from "@nestjs/common"
-// import { SequelizeModule } from "@nestjs/sequelize"
+import { SequelizeModule } from "@nestjs/sequelize"
 import { AuthController } from "./auth.controller"
 import { AuthService } from "./auth.service"
-// import { User } from "./models/user.model"
+import { User } from "./models/user.model"
 import { PassportModule } from "@nestjs/passport"
 import { JwtModule } from "@nestjs/jwt"
 import { JwtStrategy } from "./strategy/jwt.strategy"
-import { TypeOrmModule } from "@nestjs/typeorm"
-import { User } from "./models/user.entity"
-import { UserRepository } from "./models/user.repository"
 
 @Module({
   imports: [
-    // SequelizeModule.forFeature([User]),
-    TypeOrmModule.forFeature([User]),
+    SequelizeModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: "jwt" }),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
@@ -23,7 +19,7 @@ import { UserRepository } from "./models/user.repository"
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, UserRepository],
+  providers: [AuthService, JwtStrategy],
   exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,18 +1,15 @@
 import { Module } from "@nestjs/common"
-import { TypeOrmModule } from "@nestjs/typeorm"
 import { AuthModule } from "src/auth/auth.module"
 import { CategoryController } from "./category.controller"
 import { CategoryService } from "./category.service"
-import { CategoryRepository } from "./models/category.repository"
-import { Category } from "./models/category.entity"
-// import { SequelizeModule } from "@nestjs/sequelize"
-// import { Category } from "./entity/category.model"
-// import { Event } from "src/events/models/event.model"
+import { SequelizeModule } from "@nestjs/sequelize"
+import { Event } from "src/events/models/event.model"
+import { Category } from "./models/category.model"
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category]), AuthModule],
+  imports: [SequelizeModule.forFeature([Category]), AuthModule],
   controllers: [CategoryController],
-  providers: [CategoryService, CategoryRepository],
+  providers: [CategoryService],
   // exports: [SequelizeModule],
 })
 export class CategoryModule {}
